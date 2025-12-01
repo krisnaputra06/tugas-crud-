2ec9996cef978f90d655a03700e5a9f68dbbfe04
krisnaputra06 <krisnaputra0666@gmail.com>
2025-12-01 20:12:46 +0800

    first project

---
+++ views/index.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . '/../controllers/BookController.php';
+$controller = new BookController();
+$books = $controller->index();
+?>
+
+<h2>Daftar Buku</h2>
+<a href="create.php">Tambah Buku</a>
+
+<table border="1" cellpadding="8">
+    <tr>
+        <th>ID</th><th>Judul</th><th>Penulis</th><th>Tahun</th><th>Kategori</th><th>Cover</th><th>Status</th><th>Aksi</th>
+    </tr>
+
+    <?php foreach ($books as $b): ?>
+    <tr>
+        <td><?= $b['id'] ?></td>
+        <td><?= $b['judul'] ?></td>
+        <td><?= $b['penulis'] ?></td>
+        <td><?= $b['tahun_terbit'] ?></td>
+        <td><?= $b['kategori'] ?></td>
+        <td><img src="../uploads/<?= $b['cover'] ?>" width="60"></td>
+        <td><?= $b['status'] ?></td>
+        <td>
+            <a href="edit.php?id=<?= $b['id'] ?>">Edit</a>
+            <a href="delete.php?id=<?= $b['id'] ?>" onclick="return confirm('Yakin hapus?')">Delete</a>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
